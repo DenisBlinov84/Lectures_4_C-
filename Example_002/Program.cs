02506b936eb93a060b646f1d6f6f085a00886662
DENIS BLINOV <blinovda1@mail.ru>
2022-05-28 17:09:51 +0300

    add ex2

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рекурсия
+
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// Последовательность Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n)=f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 39; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
+
